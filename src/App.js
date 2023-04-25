700b02c0213d76496387ddb3be169a118919dc85
Sandra Jasmin <sandra-jasmin@hotmail.com>
2023-04-25 13:58:15 +0200

    Create react app and add tailwindcss, prettier, eslint and cleaned up files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="bg-slate-300">Test</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
