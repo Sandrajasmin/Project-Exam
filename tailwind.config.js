239dffe792a16ae6f630a57c1b3fb3ec688b9342
Sandra Jasmin <sandra-jasmin@hotmail.com>
2023-04-25 21:22:24 +0200

    Added colors and fonts to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        black: '#012030',
+        blue: '#13678A',
+        bluegreen: '#45C4B0',
+        green: '#45C4B0',
+        lightgreen: '#DAFDBA',
+        darkgrey: '#554E4E',
+        white: '#FFFFFF',
+        lightgrey: 'E5E7EB'
+      },
+      fontFamily: {
+        heading: ['Roboto', 'sans-serif'],
+        subheading: ['Cormorant Garamond', 'serif'],
+        body: ['Hind Madurai', 'sans-serif']
+      }
+    }
   },
   plugins: []
 };
